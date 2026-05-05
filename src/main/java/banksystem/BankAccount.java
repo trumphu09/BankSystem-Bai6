fc61414203ee01177c925198f8dc6322e181b274
Phan Xuan Thanh <25020397@vnu.edu.vn>
2026-05-05 11:01:10 +0700

    Lan push 2: Da co cache, toc do build cuc nhanh

--- src/main/java/banksystem/BankAccount.java
+++ src/main/java/banksystem/BankAccount.java
@@ -2,6 +2,6 @@ package banksystem;
 
 public class BankAccount {
     public void helloCaching() {
-        System.out.println("Test Caching lan 1");
+        System.out.println("Test Caching lan 2");
     }
 }
